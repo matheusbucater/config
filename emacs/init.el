4bb976a755fc068633f7377fe60729463c5e54af
Matheus Pelegrini Bucater <matheusbucater@ufu.br>
2024-03-27 14:39:19 -0300

    emacs config

---
+++ emacs/init.el
@@ -0,0 +1,17 @@
+(org-babel-load-file
+ (expand-file-name
+  "config.org"
+  user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("0820ae262720d2cf4d58947ff2e1f882e7c72d618b940101f4d02e5dcc406ca6" default)))
+;; (custom-set-faces
+;;  ;; custom-set-faces was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  )
